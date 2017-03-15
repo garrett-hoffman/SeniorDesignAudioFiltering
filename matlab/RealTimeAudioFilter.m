f341d29b23b80fd44fd9dcd444c93b94e4b5da12
garrett.m.hoffman <garrett.m.hoffman@vanderbilt.edu>
2017-03-15 11:22:38 -0500

    added comments

--- matlab/RealTimeAudioFilter.m
+++ matlab/RealTimeAudioFilter.m
@@ -1,7 +1,12 @@
+% Filters out target frequency ranges from a microphone input and sends the filtered signal to the audio output
+% Also writes the clean input signal and the filtered output signals to .wav files for later analysis
 function RealTimeAudioFilter
+
+% setup audio input and output devices
 deviceReader = audioDeviceReader;
 deviceWriter = audioDeviceWriter('SampleRate',deviceReader.SampleRate);
 
+% setup clean and filtered output files
 cleanWriter = dsp.AudioFileWriter(...
     'cleanOutput1.wav',...
     'FileFormat','WAV');
@@ -9,33 +14,40 @@ filteredWriter = dsp.AudioFileWriter(...
     'filteredOutput1.wav',...
     'FileFormat','WAV');
 
-% filt2881 = filtIIRelliptic(2700,2830,2925,3100);
-% filt960 = filtIIRelliptic(920,930,985,1000);
-% filt1920 = filtIIRelliptic(1880,1900,1940,1960);
-% filt5760 = filtIIRelliptic(5300,5500,5900,6000);
-% filt3840 = filtIIRelliptic(3650,3750,3950,4000);
-% filt4802 = filtIIRelliptic(4760,4780,4820,4840);
-
+% nyquist frequency, half the sampling rate
 nyq = 44100/2.0;
 filtType = 'stop';
+
+% tuples of coefficients to pass to the filter() function
 [a2881,b2881] = ellip(5,1,60,[2830 2925]/nyq,filtType);
 [a960,b960] = ellip(5,1,60,[930 985]/nyq,filtType);
 [a1920,b1920] = ellip(5,1,60,[1900 1940]/nyq,filtType);
 %[a5760,b5760] = ellip(5,1,60,[5500 5900]/nyq,filtType);
 %[a3840,b3840] = ellip(5,1,60,[3750 3950]/nyq,filtType);
 %[a4802,b4802] = ellip(5,1,60,[4780 4820]/nyq,filtType);
+
 disp('Begin Signal Input...')
+
+% loop takes 1000 sets of samples for testing purposes, can be changed to "while 1" for deployment
 i = 0;
 while i < 1000
+    % reads set of samlples from input 
     mySignal = deviceReader.step();
+    % writes input to clean output file
     cleanWriter.step(amplify(mySignal,4));
+
+    % filters applied in series
     myProcessedSignal = filter(a2881,b2881,mySignal);
     myProcessedSignal1 = filter(a960,b960,myProcessedSignal);
     myProcessedSignal2 = filter(a1920,b1920,myProcessedSignal1);
     %myProcessedSignal3 = filter(a5760,b5760,myProcessedSignal2);
     %myProcessedSignal4 = filter(a3840,b3840,myProcessedSignal3);
     %myProcessedSignal5 = filter(a4802,b4802,myProcessedSignal4);
+    
+    % boost output signal
     myLouderSignal = amplify(myProcessedSignal2,4);
+
+    % write to filtered output file and output device
     filteredWriter.step(myLouderSignal);
     deviceWriter.step(myLouderSignal);
     i=i+1;
